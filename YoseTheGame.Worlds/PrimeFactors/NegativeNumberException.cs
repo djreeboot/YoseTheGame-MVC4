2871a141e652cd7ce87ea5f4500146d44543686f
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:18:01 -0400

    Exception class for negative numbers

---
+++ YoseTheGame.Worlds/PrimeFactors/NegativeNumberException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YoseTheGame.Worlds.PrimeFactors
+{
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException(int number)
+            : base(string.Format("{0} is not an integer > 1", number.ToString()))
+        {
+        }
+    }
+}
