08972dfced666293b85a461569b0041a8998a469
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:31:08 -0400

    Prime factors ui script

---
+++ YoseTheGame/Scripts/PrimeFactors.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+
+    if ($.cookie("last-decomposition") != undefined) {
+        $("#last-decomposition").text($.cookie("last-decomposition"));
+        $.removeCookie("last-decomposition");
+    }
+
+    $("#go").click(function () {
+
+        $("#result").empty();
+        $("#results").empty();
+
+        var values = $("#number").val().split(",");
+        var multipleValues = (values.length > 1);
+        var url = "/primeFactors?";
+
+        for (var i = 0; i < values.length; i++) {
+            if (i != values.length - 1) {
+                url += "number=" + values[i].trim() + "&";
+            }
+            else {
+                url += "number=" + values[i].trim();
+            }
+        }
+
+        $.getJSON(url, function (data) {
+
+            if (multipleValues) {
+                displayMultipleResults(data, "#results");
+            }
+            else {
+                displaySingleResult(data, "#result");
+                $.cookie("last-decomposition", $("#result").text(), { expires: 7 });
+            }
+
+        });
+
+
+    });
+});
+
+function displaySingleResult(data, target) {
+
+    if (data.error != undefined) {
+
+        if (data.error == "not a number") {
+            $(target).text(data.number + " is " + data.error);
+        }
+        else {
+            $(target).text(data.error);
+        }
+    }
+    else {
+        var result = data.number + " = " + data.decomposition.join(" x ");
+        $(target).text(result);
+    }
+}
+
+function displayMultipleResults(data, target) {
+
+    for (var i = 0; i < data.length; i++) {
+        var html = $("<li></li>").attr("id", "result_" + i);
+        html.appendTo(target);
+        displaySingleResult(data[i], "#result_" + i);
+    }
+}
