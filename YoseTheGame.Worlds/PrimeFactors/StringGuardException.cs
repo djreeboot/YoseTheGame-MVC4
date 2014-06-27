6daa7977d1f6991c6de86c2f697371867351b74c
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:18:57 -0400

    Exception class used when a string is specified

---
+++ YoseTheGame.Worlds/PrimeFactors/StringGuardException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace YoseTheGame.Worlds.PrimeFactors
+{
+    public class StringGuardException : Exception 
+    {
+        public StringGuardException(): base("not a number")
+        {
+        }
+    }
+}
