1b71e43cf83c939d9dd94e2b3c9c6734fc50cf9e
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:22:36 -0400

    Added MinesweeperWorker class to generate random grid data

---
+++ YoseTheGame.Worlds/Minesweeper/MinesweeperWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoseTheGame.Worlds.Minesweeper
+{
+    public class MinesweeperWorker
+    {
+        public static List<List<string>> GetRandomGrid()
+        {
+            List<List<string>> grid = new List<List<string>>();
+            for (int y = 0; y < 8; y++)
+            {
+                List<string> row = new List<string>();
+                Random random = new Random(DateTime.Now.Millisecond);
+                for (int x = 0; x < 8; x++)
+                {
+                    row.Add(random.Next(0, 10) <= 2 ? "bomb" : "empty");
+                }
+                grid.Add(row);
+            }
+            return grid;
+        }
+    }
+}
