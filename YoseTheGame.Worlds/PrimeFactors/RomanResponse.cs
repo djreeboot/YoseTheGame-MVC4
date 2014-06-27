22e7dc4d852e938e44c22d1fe2efe5685fb5954b
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:20:19 -0400

    Response object for roman decomposition

---
+++ YoseTheGame.Worlds/PrimeFactors/RomanResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace YoseTheGame.Worlds.PrimeFactors
+{
+    public class RomanResponse : Response
+    {
+        public List<string> decomposition { get; set; }
+
+        public RomanResponse(string number, List<string> decomposition)
+        {
+            this.number = number;
+            this.decomposition = decomposition;
+        }
+    }
+}
