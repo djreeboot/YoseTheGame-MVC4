75380f6a34bc2606c1e07d85bc7c380b4f0a4f8d
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:32:19 -0400

    Initial commit.

---
+++ YoseTheGame/Controllers/MinesweeperController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Collections.Generic;
+using YoseTheGame.Worlds.Minesweeper;
+
+namespace YoseTheGame.Controllers
+{
+    public class MinesweeperController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Minesweeper");
+        }
+
+        public ActionResult Data()
+        {
+            return Json(MinesweeperWorker.GetRandomGrid(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
