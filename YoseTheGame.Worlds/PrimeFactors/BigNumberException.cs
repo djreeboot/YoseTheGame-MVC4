cbd6864d0d47d8a189d26a1a72150c6716769709
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:17:16 -0400

    Exception class for big numbers

---
+++ YoseTheGame.Worlds/PrimeFactors/BigNumberException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace YoseTheGame.Worlds.PrimeFactors
+{
+    public class BigNumberException : Exception
+    {
+        public BigNumberException() : base("too big number (>1e6)")
+        {
+        }
+    }
+}
