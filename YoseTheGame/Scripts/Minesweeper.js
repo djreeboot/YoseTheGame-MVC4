79397d466b246e044054faefa08d291406c3d0f5
Benoit Champagne <bchampagne@victrix.ca>
2014-06-26 22:31:28 -0400

    Minesweeper javascript.

---
+++ YoseTheGame/Scripts/Minesweeper.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+
+    load();
+    
+});
+
+function load() {
+
+    var url = "/minesweeper/data";
+    $.getJSON(url, function (data) {
+        document.grid = data;
+    });
+    registerClick();
+}
+
+function registerClick() {
+
+    $("td").click(function () {
+        var row = cellRow(this);
+        var col = cellCol(this);
+        if (document.grid[row][col] == "bomb") {
+            $(this).addClass("lost");
+        }
+        else {
+            $(this).addClass("safe");
+        }
+    });
+}
+
+function cellRow(cell) {
+    return parseInt(cell.id.substring(5, 6)) - 1;
+}
+
+function cellCol(cell) {
+    return parseInt(cell.id.substring(7, 8)) - 1;
+}
