b6c0f11631f36869fc02f8a2cc7b8aec0cef3a55
Benoit Champagne <bchampagne@victrix.ca>
2014-06-27 21:28:58 -0400

    Added last decomposition persistence

--- YoseTheGame/Controllers/PrimeFactorsController.cs
+++ YoseTheGame/Controllers/PrimeFactorsController.cs
@@ -5,22 +5,30 @@ namespace YoseTheGame.Controllers
 {
     public class PrimeFactorsController : Controller
     {
+        public static string LastDecomposition = string.Empty;
+
         public ActionResult Index(string number)
         {
             if (Request != null)
             {
                 string[] values = Request.QueryString["number"].Split(',');
-                
-                if (values.Length > 1)
-                    return Json(PrimeFactorsWorker.Decompose((object)values), JsonRequestBehavior.AllowGet);
-            }
 
+                if (values.Length > 1)
+                {
+                    return Json(PrimeFactorsWorker.Decompose((object)values), JsonRequestBehavior.AllowGet);
+                }
+            }
             return Json(PrimeFactorsWorker.Decompose((object)number), JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Form()
         {
             return View("PrimeFactors");
+        }
+
+        public void SetLastDecomposition(string value)
+        {
+            PrimeFactorsController.LastDecomposition = value;
         }
     }
 }
