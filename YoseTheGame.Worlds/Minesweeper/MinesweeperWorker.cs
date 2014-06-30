774b0363fb6bf587f58b1d156ccb088af7deec5d
Benoit Champagne <bchampagne@victrix.ca>
2014-06-30 08:15:40 -0400

    Improved grid data randomizer

--- YoseTheGame.Worlds/Minesweeper/MinesweeperWorker.cs
+++ YoseTheGame.Worlds/Minesweeper/MinesweeperWorker.cs
@@ -11,13 +11,15 @@ namespace YoseTheGame.Worlds.Minesweeper
         public static List<List<string>> GetRandomGrid()
         {
             List<List<string>> grid = new List<List<string>>();
+            Random random = new Random(Environment.TickCount);
+
             for (int y = 0; y < 8; y++)
             {
-                List<string> row = new List<string>();
-                Random random = new Random(DateTime.Now.Millisecond);
+                List<string> row = new List<string>();    
                 for (int x = 0; x < 8; x++)
                 {
-                    row.Add(random.Next(0, 10) <= 2 ? "bomb" : "empty");
+                    int value = random.Next(0, 1000);
+                    row.Add((value <= 200) ? "bomb" : "empty");
                 }
                 grid.Add(row);
             }
