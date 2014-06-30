850c73a51c7cc7d173f9891a56fa3a8d5f1260aa
Benoit Champagne <bchampagne@victrix.ca>
2014-06-30 11:56:45 -0400

    Added tests for the minesweeper controller actions

---
+++ YoseTheGame.Tests/MinesweeperControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YoseTheGame.Controllers;
+
+namespace YoseTheGame.Tests
+{
+    [TestClass]
+    public class MinesweeperControllerTests
+    {
+        private MinesweeperController _controller;
+
+        [TestInitialize]
+        public void ThisController()
+        {
+            _controller = new MinesweeperController();
+        }
+
+        [TestMethod]
+        public void IndexActionReturnsTheExpectedView()
+        {
+            ActionResult result = _controller.Index();
+            var viewName = ((ViewResult)result).ViewName;
+            Assert.IsTrue(viewName == "Minesweeper");
+        }
+
+        [TestMethod]
+        public void DataActionReturnsJson()
+        {
+            ActionResult result = _controller.Data();
+            Assert.AreSame(result.GetType(), typeof(JsonResult));
+        }
+    }
+}
